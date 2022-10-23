9b959efd8415985157414a8133168a12e4fad45c
Matheus Pitsch <101012809+MatheusPitsch@users.noreply.github.com>
2022-10-23 14:15:06 -0300

    new folder BetyBankAdm

---
+++ BetyBankAdm/Funcionarios/Funcionario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetyBankAdm.Funcionarios
+{
+    public class Funcionario
+    {
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public decimal Wage { get; set; }
+
+        public decimal Bonus()
+        {
+            return this.Wage * 10 / 100;
+        }
+    }
+}
